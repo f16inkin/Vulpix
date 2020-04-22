c6d25ca5f7bfdd9dbb211a3e279676b14b7e7516
Rain <f16nkin@gmail.com>
2020-04-22 16:27:35 +1000

    #27 RBAC Responders upload

---
+++ src/Engine/RBAC/Responders/GetPermissionsResponder.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Vulpix\Engine\RBAC\Responders;
+
+
+use Laminas\Diactoros\Response;
+use Laminas\Diactoros\Response\JsonResponse;
+use Psr\Http\Message\ServerRequestInterface;
+
+class GetPermissionsResponder
+{
+    public function respond(ServerRequestInterface $request, $payload = null): Response
+    {
+        return new JsonResponse($payload);
+    }
+}
