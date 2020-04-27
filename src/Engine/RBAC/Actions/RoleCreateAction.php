8990a5edc48bd20e8cc757e5e82e95fd8b84222e
Rain <f16nkin@gmail.com>
2020-04-27 14:58:59 +1000

    #41 RBAC Создание роли завершено.

--- src/Engine/RBAC/Actions/RoleCreateAction.php
+++ src/Engine/RBAC/Actions/RoleCreateAction.php
@@ -5,18 +5,27 @@ declare(strict_types = 1);
 namespace Vulpix\Engine\RBAC\Actions;
 
 
-use Laminas\Diactoros\Response\JsonResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Vulpix\Engine\RBAC\Domains\RBACExceptionsHandler;
 use Vulpix\Engine\RBAC\Domains\Role;
 use Vulpix\Engine\RBAC\Responders\RoleCreateResponder;
 
+/**
+ * Class RoleCreateAction
+ * @package Vulpix\Engine\RBAC\Actions
+ */
 class RoleCreateAction implements RequestHandlerInterface
 {
     private $_role;
     private $_responder;
 
+    /**
+     * RoleCreateAction constructor.
+     * @param Role $role
+     * @param RoleCreateResponder $responder
+     */
     public function __construct(Role $role, RoleCreateResponder $responder)
     {
         $this->_role = $role;
@@ -31,34 +40,13 @@ class RoleCreateAction implements RequestHandlerInterface
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         try{
-            $postData = json_decode(file_get_contents("php://input"),true);
-            $roleName = $postData['roleName'] ?: '';
-            $roleDescription = $postData['roleDescription'];
-            /**
-             * Если параметры пустые или null сервер ответи 400 - Bad Request
-             */
-            if ($roleName !== '' AND $roleName !== null){
-                /**
-                 * Если роль уже присутсвует сервер ответит 409 - Conflict
-                 */
-                if (!$this->_role->isROleExist($roleName)){
-                    /**
-                     * Иначе вернет 201 - Created и сразу созданный ресурс, а так же ссылку на него.
-                     */
-                    $roleID = $this->_role->create($roleName, $roleDescription);
-                    $role = $this->_role->read($roleID);
-                    $payload['status'] = 201;
-                    $payload['resource'] = $role;
-                }else{
-                    $payload['status'] = 409;
-                }
-            }else{
-                $payload['status'] = 400;
-            }
-            $response = $this->_responder->respond($request, $payload);
+            $postData = json_decode(file_get_contents("php://input"),true) ?: null;
+            $exec = $this->_role->create($postData);
+            $role = $this->_role->get($exec->_body);
+            $response = $this->_responder->respond($request, $exec->setBody($role));
             return $response;
-        }catch (\PDOException $e){
-            return new JsonResponse(['Ошибка работы БД' => $e->getMessage()], 500);
+        }catch (\Exception $e){
+            return (new RBACExceptionsHandler())->handle($e);
         }
 
     }
--- src/Engine/RBAC/Responders/RoleCreateResponder.php
+++ src/Engine/RBAC/Responders/RoleCreateResponder.php
@@ -8,17 +8,18 @@ namespace Vulpix\Engine\RBAC\Responders;
 use Laminas\Diactoros\Response;
 use Laminas\Diactoros\Response\JsonResponse;
 use Psr\Http\Message\ServerRequestInterface;
+use Vulpix\Engine\Core\DataStructures\ExecutionResponse;
 
 class RoleCreateResponder
 {
-    public function respond(ServerRequestInterface $request, $payload = null): Response
+    public function respond(ServerRequestInterface $request, ExecutionResponse $payload): Response
     {
-        $response = new JsonResponse($payload['resource'], $payload['status']);
+        $response = new JsonResponse($payload->_body, $payload->_status);
         /**
          * Местоположение записываю в Location, при условии что ресурс создан.
          */
-        if (isset($payload['resource'])){
-            $response = $response->withHeader('Location', '/api/v1/roles/'.$payload['resource']->_id);
+        if ($payload->_status === 201){
+            $response = $response->withHeader('Location', '/api/v1/roles/'.$payload->_body->_id);
         }
         return $response;
     }
