ba663ffb882856d15ba97d7e1de5089c25c0a633
Rain <f16nkin@gmail.com>
2020-04-28 14:08:08 +1000

    #45 RBAC Дополнил PermissionCollection методом сериализации в JSON.

--- src/Engine/RBAC/Domains/PermissionCollection.php
+++ src/Engine/RBAC/Domains/PermissionCollection.php
@@ -7,7 +7,11 @@ namespace Vulpix\Engine\RBAC\Domains;
 
 use Vulpix\Engine\Database\Connectors\IConnector;
 
-class PermissionCollection
+/**
+ * Class PermissionCollection
+ * @package Vulpix\Engine\RBAC\Domains
+ */
+class PermissionCollection implements \JsonSerializable
 {
     private $_permissions = [];
     private $_dbConnector;
@@ -35,4 +39,15 @@ class PermissionCollection
         return $this;
     }
 
+    /**
+     * Specify data which should be serialized to JSON
+     * @link https://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4.0
+     */
+    public function jsonSerialize()
+    {
+        return $this->_permissions;
+    }
 }
