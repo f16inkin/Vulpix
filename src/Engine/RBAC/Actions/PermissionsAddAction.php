e9415a2be4a3ea04788b37f21a2446a334cc8e5f
Rain <f16nkin@gmail.com>
2020-04-28 10:04:36 +1000

    #44 RBAC Переделал на новый формат с Execution Response механизм добавления привелегий роли.

--- src/Engine/RBAC/Actions/PermissionsAddAction.php
+++ src/Engine/RBAC/Actions/PermissionsAddAction.php
@@ -5,11 +5,11 @@ declare(strict_types = 1);
 namespace Vulpix\Engine\RBAC\Actions;
 
 
-use Laminas\Diactoros\Response\JsonResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Vulpix\Engine\RBAC\Domains\PermissionManager;
+use Vulpix\Engine\RBAC\Domains\RBACExceptionsHandler;
 use Vulpix\Engine\RBAC\Domains\Role;
 use Vulpix\Engine\RBAC\Responders\PermissionsAddResponder;
 
@@ -37,6 +37,9 @@ class PermissionsAddAction implements RequestHandlerInterface
     }
 
     /**
+     * По итогу исполнения вернет Роль в которую добавлялись привелегии в независимости от результата.
+     * Добавились или нет.
+     *
      * Handles a request and produces a response.
      *
      * May call other collaborating code to generate the response.
@@ -48,21 +51,25 @@ class PermissionsAddAction implements RequestHandlerInterface
              * Учитывается так же ситуация, когда на маршрут не отправлены параметры.
              * Если так случилось будет возврашен JSON с NULL параметрами.
              */
-            $postData = json_decode(file_get_contents("php://input"),true);
-            $roleId = (int)$postData['roleId'];
+            $postData = json_decode(file_get_contents("php://input"),true) ?: null;
+            $roleId = (int)$postData['roleId'] ?: null;
             //Добавляемые привелегии
-            $addingPermissionsIDs = $postData['permissionIDs'] ?: [];
+            $addingPermissionsIDs = $postData['permissionIDs'];
             //Найденные привелегии для текущей роли
             $foundPermissionIDs = $this->_manager->findRolePermissionIDs($roleId, $addingPermissionsIDs);
             //Те привелегии которые добавляются, которых нет в имеющихся
             $permissionsIDs = array_diff($addingPermissionsIDs, $foundPermissionIDs);
-            $roleId = $this->_manager->addPermissions($roleId, $permissionsIDs);
+            /**
+             * Вернет либо 201 либо 200 статус в результате выполнения. На клиенете будет проще различать по статусу
+             * были ли добавлены првиелегии, либо запрос прошел и добавляемые првиелегии уже были у роли.
+             */
+            $exec = $this->_manager->addPermissions($roleId, $permissionsIDs);
             //Полная ифнормация по роли
-            $role = $this->_role->read($roleId);
-            $response = $this->_responder->respond($request, $role);
+            $role = $this->_role->get($exec->_body);
+            $response = $this->_responder->respond($request, $exec->setBody($role));
             return $response;
-        }catch (\PDOException $e){
-            return new JsonResponse(['Ошибка работы БД' => $e->getMessage()], 500);
+        }catch (\Exception $e){
+            return (new RBACExceptionsHandler())->handle($e);
         }
     }
 }
--- src/Engine/RBAC/Responders/PermissionsAddResponder.php
+++ src/Engine/RBAC/Responders/PermissionsAddResponder.php
@@ -8,11 +8,12 @@ namespace Vulpix\Engine\RBAC\Responders;
 use Laminas\Diactoros\Response;
 use Laminas\Diactoros\Response\JsonResponse;
 use Psr\Http\Message\ServerRequestInterface;
+use Vulpix\Engine\Core\DataStructures\ExecutionResponse;
 
 class PermissionsAddResponder
 {
-    public function respond(ServerRequestInterface $request, $payload = null): Response
+    public function respond(ServerRequestInterface $request, ExecutionResponse $payload): Response
     {
-        return new JsonResponse($payload, 201);
+        return new JsonResponse($payload->_body, $payload->_status);
     }
 }
