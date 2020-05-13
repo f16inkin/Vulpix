60b0dc721d7a9682b887f655ec2f2934d4451263
Rain <Valehtelit@mail.ru>
2020-05-13 20:31:12 +1000

    #71 Test Подгрузил первые тесты.

---
+++ tests/Engine/AAIS/DataStructures/ValueObjects/AccessTokenTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Engine\AAIS\DataStructures\ValueObjects;
+
+use PHPUnit\Framework\TestCase;
+use Vulpix\Engine\AAIS\DataStructures\ValueObjects\AccessToken;
+
+class AccessTokenTest extends TestCase
+{
+    public function testSuccess() : void {
+        $token = new AccessToken($value = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9');
+        self::assertEquals($value, $token->getValue());
+    }
+
+    public function testEmpty() : void
+    {
+        $this->expectException( \InvalidArgumentException::class);
+        $token = new AccessToken('');
+    }
+
+    public function testNull() : void
+    {
+        $this->expectException( \InvalidArgumentException::class);
+        $token = new AccessToken(null);
+    }
+
+
+
+}
